66dcac087b572816e9907325d120f6695967049c
Zingabopp <37518403+Zingabopp@users.noreply.github.com>
2019-03-08 15:06:03 -0600

    Clean up and documentation

--- Command-Interface/CommandBehavior.cs
+++ Command-Interface/CommandBehavior.cs
@@ -15,42 +15,14 @@ namespace Command_Interface
     public class CommandBehavior : WebSocketBehavior
     {
         private CIHTTPServer _server;
-        private List<ICommandPlugin> _registeredPlugins;
-        private List<ICommandPlugin> RegisteredPlugins
-        {
-            get
-            {
-                if (_registeredPlugins == null)
-                    return _registeredPlugins = new List<ICommandPlugin>();
-                return _registeredPlugins;
-            }
-        }
 
         public void Setup(CIHTTPServer server)
         {
             _server = server;
             _server.MessageReady += onMessageReady;
+            
             Console.WriteLine("CommandBehavior Setup()");
-            SceneManager.activeSceneChanged += OnSceneChange;
-        }
-
-        public void RegisterPlugin(ICommandPlugin newPlug)
-        {
-            if (!RegisteredPlugins.Contains(newPlug))
-            {
-                RegisteredPlugins.Add(newPlug);
-                newPlug.MessageReady += onMessageReady;
-            }
-            else
-            {
-                Logger.Debug($"Unable to register plugin: {newPlug.PluginName} is already registered");
-            }
-        }
-
-        public void DeRegisterPlugin(ICommandPlugin plug)
-        {
-            RegisteredPlugins.Remove(plug);
-            plug.MessageReady -= onMessageReady;
+            //SceneManager.activeSceneChanged += OnSceneChange;
         }
 
         /// <summary>
@@ -59,7 +31,10 @@ namespace Command_Interface
         /// <param name="e"></param>
         protected override void OnMessage(MessageEventArgs e)
         {
+            Logger.Trace($"Recieved a message: {e.Data}");
             var msg = e.Data.ToMessageData();
+
+            Logger.Trace($"Received a message:\n{msg.ToString(3)}");
             if (_server.Plugins.ContainsKey(msg.Destination))
                 _server.Plugins[msg.Destination].OnMessage(this, msg);
             else
@@ -71,41 +46,39 @@ namespace Command_Interface
         /// Message ready to send to server.
         /// </summary>
         /// <param name="msg"></param>
-        public void onMessageReady(MessageData msg)
+        public void onMessageReady(object sender, MessageData msg)
         {
             Logger.Debug($"Message ready:\n{msg.ToString(3)}");
             Send(msg.ToJSON());
         }
 
+        /// <summary>
+        /// Connection is closing, unsubscribe from the server's MessageReady event.
+        /// </summary>
+        /// <param name="e"></param>
         protected override void OnClose(CloseEventArgs e)
         {
             Console.WriteLine($"Connection closed because: {e.Reason}");
-            SceneManager.activeSceneChanged -= OnSceneChange;
+            //SceneManager.activeSceneChanged -= OnSceneChange;
             _server.MessageReady -= onMessageReady;
-            RegisteredPlugins.ForEach(p => DeRegisterPlugin(p));
+            //_server.Plugins.Values.ToList().ForEach(p => DeRegisterPlugin(p));
             base.OnClose(e);
         }
 
+        /*
         public void OnSceneChange(Scene oldScene, Scene newScene)
         {
             try
             {
                 if (newScene.name == "Menu")
                 {
-                    _server.CheckPlugins();
-                    //Code to execute when entering The Menu
-                    //var testMessage = new MessageData("OBSControl", "OBSControl", "", "STOPREC");
-                    //Logger.Trace($"In menu, sending message:\n{testMessage.ToString()}");
-                    //Send(testMessage.ToJSON());
+
 
                 }
 
                 if (newScene.name == "GameCore")
                 {
-                    //Code to execute when entering actual gameplay
-                    //var testMessage = new MessageData("OBSControl", "OBSControl", "", "STARTREC");
-                    //Logger.Trace($"In GameCore, sending message:\n{testMessage.ToString()}");
-                    //Send(testMessage.ToJSON());
+
 
                 }
             }
@@ -115,10 +88,6 @@ namespace Command_Interface
             }
 
         }
-
-        public void OnSendComplete(bool finished)
-        {
-            Console.WriteLine($"Completed? {finished}");
-        }
+        */
     }
 }
--- Command-Interface/HTTPServer.cs
+++ Command-Interface/HTTPServer.cs
@@ -28,29 +28,114 @@ namespace Command_Interface
         private CommandBehavior _comService;
         private Dictionary<string, ICommandPlugin> _plugins;
 
-        public event Action<MessageData> MessageReady;
+        /// <summary>
+        /// Event fired when there is a message to send to the Command-Server.
+        /// </summary>
+        public event Action<object, MessageData> MessageReady;
 
+        /// <summary>
+        /// Dictionary of currently registered plugins.
+        /// </summary>
         public Dictionary<string, ICommandPlugin> Plugins
         {
             get
             {
-                var plugins = this.GetComponents<ICommandPlugin>().Where(p => !(p is CIHTTPServer)).ToList();
-                var dict = new Dictionary<string, ICommandPlugin>();
-                foreach (var plug in plugins)
+                if (_plugins == null)
+                    _plugins = new Dictionary<string, ICommandPlugin>();
+                else
                 {
-                    Logger.Trace($"Adding component {plug.PluginName} to dictionary");
-                    dict.AddSafe(plug.PluginName, plug);
-                    _comService.RegisterPlugin(plug);
+                    // Check for plugins that don't exist and remove them
+                    foreach (var pair in _plugins)
+                    {
+                        if (pair.Value == null)
+                            _plugins.Remove(pair.Key);
+                    }
                 }
-                return dict;
+                return _plugins;
             }
         }
 
         public string PluginName => Plugin.PluginName;
 
-        public Dictionary<string, Action<string>> Commands => throw new NotImplementedException();
+        private Dictionary<string, Action<object, string>> _commands;
 
-        private void Start()
+        /// <summary>
+        /// Dictionary of commands that can be handled by the server.
+        /// </summary>
+        public Dictionary<string, Action<object, string>> Commands
+        {
+            get
+            {
+                if (_commands == null)
+                    _commands = new Dictionary<string, Action<object, string>>();
+                return _commands;
+            }
+        }
+
+        private void BuildCommands()
+        {
+
+            Commands.Add("REGISTER", RegisterPlugin);
+            Commands.Add("DEREGISTER", DeRegisterPlugin);
+
+        }
+
+        /// <summary>
+        /// Register the plugin with the server.
+        /// TODO: If key is already registered, check if plugin reference is the same and reply if they aren't. (Resolve two plugins trying to use same name).
+        /// </summary>
+        /// <param name="plugin"></param>
+        /// <param name="name"></param>
+        public void RegisterPlugin(object plugin, string name)
+        {
+            Logger.Debug($"Registering plugin {name}");
+
+            if (plugin == null)
+            {
+                Logger.Error("Plugin is null, unable to register");
+                return;
+            }
+            ICommandPlugin plug = plugin as ICommandPlugin;
+            if (plug != null)
+            {
+                try
+                {
+                    Plugins.AddSafe(plug.PluginName, plug);
+                    plug.MessageReady += OnMessage;
+                }
+                catch (Exception ex)
+                {
+                    Logger.Exception("Error registering plugin", ex);
+                }
+
+            }
+            else
+                Logger.Error("Tried to register null plugin");
+
+        }
+
+        /// <summary>
+        /// Unregisters the plugin. The plugin will no longer be sent messages, and the server won't receive messages from the plugin.
+        /// </summary>
+        /// <param name="plugin"></param>
+        /// <param name="name"></param>
+        public void DeRegisterPlugin(object plugin, string name)
+        {
+            Logger.Debug($"Registering plugin {name}");
+            var plug = plugin as ICommandPlugin;
+            if (plug != null)
+            {
+                Plugins.Remove(plug.PluginName);
+                plug.MessageReady -= OnMessage;
+            }
+            else
+                Logger.Error("Tried to register null plugin");
+        }
+
+        /// <summary>
+        /// Initialize the server.
+        /// </summary>
+        private void Awake()
         {
             Logger.Debug("Starting CIHTTPServer...");
             if (_instance != null)
@@ -60,29 +145,26 @@ namespace Command_Interface
             }
             _instance = this;
             InitServer();
-            CheckPlugins();
-            
+            BuildCommands();
         }
 
-        public void CheckPlugins()
+        private void Start()
         {
-            /*
-            var plugins = this.GetComponents<ICommandPlugin>().Where(p => !(p is CIHTTPServer)).ToList();
-            var dict = new Dictionary<string, ICommandPlugin>();
-            plugins.Where(p => Plugins.ContainsKey(p.PluginName)).ToList().ForEach(plug => {
-                Logger.Trace($"Adding component {plug.PluginName} to dictionary");
-                dict.AddSafe(plug.PluginName, plug);
-                _comService.RegisterPlugin(plug);
-            });
-            */
-            var thing = Plugins.Values;
+            // Not currently used.
         }
 
+        /// <summary>
+        /// Stops the WebSocketServer when the GameObject is being destroyed.
+        /// </summary>
         private void OnDestroy()
         {
             Logger.Debug("Destroying CIHTTPServer...");
             StopServer();
         }
+
+        /// <summary>
+        /// Creates and starts a new WebSocketServer with the WebSocketBehavior that receives the messages from the server. 
+        /// </summary>
         public void InitServer()
         {
             Logger.Info("Initializing server...");
@@ -93,9 +175,17 @@ namespace Command_Interface
             });
             _server.Log.Output = (_, __) => { }; // Disable error output
             _server.Start();
-            
+            Logger.Info("Server started");
+            foreach (var plug in Plugins.Values)
+            {
+                //_comService.RegisterPlugin(plug);
+            }
+
         }
 
+        /// <summary>
+        /// Stops the server.
+        /// </summary>
         public void StopServer()
         {
             Logger.Debug("Stopping server...");
@@ -106,22 +196,50 @@ namespace Command_Interface
         /// <summary>
         /// Received message from plugin.
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        /// <param name="sender">ICommandPlugin that is sending the message</param>
+        /// <param name="e">MessageData being sent</param>
         public void OnMessage(object sender, MessageData e)
         {
-            ICommandPlugin s = (ICommandPlugin) sender;
-            if (e.Destination == "Command-Interface" && e.Data == "REGISTER")
-                _comService.RegisterPlugin(s);
-            else
-                MessageReady(e);
+            Logger.Trace($"Received a message from plugin:\n{e.ToString(3)}");
+            try
+            {
+                if (e.Destination != "Command-Interface")
+                {
+                    // Message is not for Command-Interface, pass it on
+                    Logger.Trace("Passing on message, Command-Interface is not the destination");
+                    if(MessageReady != null)
+                        MessageReady(sender, e);
+                    else
+                    {
+                        Logger.Error("CommandBehavior is not listening");
+                        //TODO: Reply system to notify plugins we're not connected to Command-Server.
+                    }
+                    return;
+                }
+                Logger.Trace($"Message is for Command-Interface:\n{e.ToString(3)}");
+                ICommandPlugin s = sender as ICommandPlugin;
+                if (s != null)
+                {
+                    // Check if the command is valid
+                    if (Commands.ContainsKey(e.Flag))
+                    {
+                        Commands[e.Flag](s, e.Data);
+                    }
+                    else
+                        Logger.Warning($"Invalid command: {e.Flag}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Exception("Exception in OnMessage", ex);
+            }
         }
 
         public void Initialize()
         {
-            throw new NotImplementedException();
+            // Not currently used.
         }
     }
 
-    
+
 }
